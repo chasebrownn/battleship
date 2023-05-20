378be25e078bec0d8f9bf993cb4051d5b061e49c
unadulterateddesign <marcus@elevatesoftware.io>
2023-05-20 10:44:50 -0700

    simple board

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
-# state variables
-board_size = 10
-empty_space = 'O'
-ship_space ='S'
-hit_space = 'X'
+from random import randint
+
+create_board = []
+for x in range(5):
+    create_board.append(("O") * 5)
+
+def display_board(create_board):
+    for row in create_board:
+        print((" ".join(row)))
+
+print("Welcome to Battleship!")
+display_board(create_board)
 
 
-# functions
-def create_board(size):
-     """Create a new game board."""
-     return [[empty_space for _ in range(size)] for _ in range(size)]
 
-def print_board(board):
-    """Print the game board"""
-    for row in board: 
-        print(" ".join(row))
-
-def place_ship(board, ship):
-    """Place a ship on the board"""
-    # for now, we'll just place the ship at a random location
-    import random
-    x = random.randint(0, board_size - 1)
-    y = random.randint(0, board_size - 1)
-    board[x][y] = ship_space
-
-def main():
-    print("Welcome to Battleship!")
-
-    # declare ships
-    # declare board
-    board = create_board(board_size)
-    # declare bot instance
-    # bot chooses ship locations
-
-    # for testing, we'll just place one ship
-    place_ship(board, ship_space)
-
-    print_board(board)
-
-main()
