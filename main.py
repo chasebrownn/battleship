3903ca533264126886a44239787e99a8eb81b412
Chase Brown <Chasebrown7@icloud.com>
2023-05-23 12:58:19 -0700

    added comments to place_ships func

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 # state variables
-board_size = 10
-empty_space = 'O'
-ship_space ='S'
-hit_space = 'X'
 
 class Ship:
     def __init__(self, shipClass, hitPoints):
@@ -12,6 +8,11 @@ class Ship:
 
 playerShips = []
 botShips = []
+
+board_size = 10
+empty_space = 'O'
+ship_space ='S'
+hit_space = 'X'
 
 # functions
 def create_board(size):
@@ -23,9 +24,14 @@ def print_board(board):
     for row in board: 
         print(" ".join(row))
 
-def place_ship(board, ship):
+def place_ships(board):
     """Place a ship on the board"""
     # for now, we'll just place the ship at a random location
+    # bot places carrier
+    # bot places battleship
+    # bot places cruiser
+    # bot places submarine
+    # bot places destroyer
     import random
     x = random.randint(0, board_size - 1)
     y = random.randint(0, board_size - 1)
@@ -43,31 +49,24 @@ def createShips():
     botShips.append(Ship("Submarine", 3))
     botShips.append(Ship("Destroyer", 2))
 
-def createShips():
-    playerShips.append(Ship("Carrier", 5))
-    playerShips.append(Ship("Battleship", 4))
-    playerShips.append(Ship("Cruiser", 3))
-    playerShips.append(Ship("Submarine", 3))
-    playerShips.append(Ship("Destroyer", 2))
-    botShips.append(Ship("Carrier", 5))
-    botShips.append(Ship("Battleship", 4))
-    botShips.append(Ship("Cruiser", 3))
-    botShips.append(Ship("Submarine", 3))
-    botShips.append(Ship("Destroyer", 2))
-
 def main():
     print("Welcome to Battleship!")
-
-    createShips()
+    print("***********************")
 
     # declare ships
+    createShips()
+    print("Ships created")
+
     # declare board
     board = create_board(board_size)
+    print("board created")
+
     # declare bot instance
+    place_ships(board)
+
     # bot chooses ship locations
 
     # for testing, we'll just place one ship
-    place_ship(board, ship_space)
 
     print_board(board)
 
