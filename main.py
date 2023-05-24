4c78eed240225ffd0b330cb98eaf36172f7bf9ae
Chase Brown <Chasebrown7@icloud.com>
2023-05-24 15:52:13 -0700

    Added constants, print_ships, and Coordinate class

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 # imports
+
 import random
+import constants
+
 
 # state variables
+
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return f"x:{self.x}, y:{self.y}"
 
 class Ship:
     def __init__(self, shipClass, hitPoints, letter):
@@ -9,6 +20,9 @@ class Ship:
         self.hitPoints = hitPoints
         self.letter = letter
         self.position = []
+    
+    def __str__(self):
+        return f"{self.shipClass}"
 
 playerShips = []
 playerHealth = 17
@@ -16,14 +30,12 @@ playerHealth = 17
 botShips = []
 botHealth = 17
 
-board_size = 10
-empty_space = 'O'
-hit_space = 'X'
 
 # functions
+
 def create_board(size):
      """Create a new game board."""
-     return [[empty_space for _ in range(size)] for _ in range(size)]
+     return [[constants.EMPTY_SPACE for _ in range(size)] for _ in range(size)]
 
 def print_board(board):
     """Print the game board"""
@@ -48,6 +60,12 @@ def print_boards(bot_board, player_board):
         print(row_num, " ".join(row))
         row_num+=1
 
+def print_ships(ships):
+    for ship in ships:
+        print(ship.shipClass)
+        for i in range(len(ship.position)):
+            print(ship.position[i])
+
 def place_ships(board):
     """Place a ship on the board"""
     # iterate through all ships in botShips
@@ -56,15 +74,15 @@ def place_ships(board):
         room = False
         while room == False:
             # choose random coordinate
-            x = random.randint(0, board_size - 1)
-            y = random.randint(0, board_size - 1)
+            x = random.randint(0, constants.BOARD_SIZE - 1)
+            y = random.randint(0, constants.BOARD_SIZE - 1)
 
             # is there room for the ship below the coordinate?
-            if x + ship.hitPoints - 1 < board_size:
+            if x + ship.hitPoints - 1 < constants.BOARD_SIZE:
                 tempX = x
                 for i in range(ship.hitPoints):
                     # check if there's an existing ship here
-                    if board[tempX][y] == empty_space:
+                    if board[tempX][y] == constants.EMPTY_SPACE:
                         room = True
                     else:
                         room = False
@@ -75,13 +93,14 @@ def place_ships(board):
                     # place ship
                     for i in range(ship.hitPoints):
                         board[tempX][y] = ship.letter
+                        ship.position.append(Coordinate(tempX, y))
                         tempX+=1
 
             # is there room for the ship to the left of the coordinate?
             elif y - ship.hitPoints - 1 >= 0:
                 tempY = y
                 for i in range(ship.hitPoints):
-                    if board[x][y] == empty_space:
+                    if board[x][y] == constants.EMPTY_SPACE:
                         room = True
                     else:
                         room = False
@@ -91,13 +110,14 @@ def place_ships(board):
                     tempY = y
                     for i in range(ship.hitPoints):
                         board[x][tempY] = ship.letter
+                        ship.position.append(Coordinate(x, tempY))
                         tempY-=1
 
             # is there room for the ship above the coordinate?
             elif x - ship.hitPoints - 1 >= 0:
                 tempX = x
                 for i in range(ship.hitPoints):
-                    if board[tempX][y] == empty_space:
+                    if board[tempX][y] == constants.EMPTY_SPACE:
                         room = True
                     else:
                         room = False
@@ -110,10 +130,10 @@ def place_ships(board):
                         tempX-=1
 
             # is there room for the ship to the right of the coordinate?
-            elif y + ship.hitPoints - 1 < board_size:
+            elif y + ship.hitPoints - 1 < constants.BOARD_SIZE:
                 tempY = y
                 for i in range(ship.hitPoints):
-                    if board[x][tempY] == empty_space:
+                    if board[x][tempY] == constants.EMPTY_SPACE:
                         room = True
                     else:
                         room = False
@@ -126,12 +146,12 @@ def place_ships(board):
                         tempY+=1
 
 def createShips():
-    playerShips.append(Ship("Carrier", 5, "C"))
+    playerShips.append(Ship("Carrier", 5, "A"))
     playerShips.append(Ship("Battleship", 4, "B"))
     playerShips.append(Ship("Cruiser", 3, "C"))
     playerShips.append(Ship("Submarine", 3, "S"))
     playerShips.append(Ship("Destroyer", 2, "D"))
-    botShips.append(Ship("Carrier", 5, "C"))
+    botShips.append(Ship("Carrier", 5, "A"))
     botShips.append(Ship("Battleship", 4, "B"))
     botShips.append(Ship("Cruiser", 3, "C"))
     botShips.append(Ship("Submarine", 3, "S"))
@@ -146,17 +166,18 @@ def main():
     print("Ships created")
 
     # declare bot board
-    bot_board = create_board(board_size)
+    bot_board = create_board(constants.BOARD_SIZE)
     print("bot board created")
 
     # declare player board
-    player_board = create_board(board_size)
-    guess_board = create_board(board_size)
+    player_board = create_board(constants.BOARD_SIZE)
+    guess_board = create_board(constants.BOARD_SIZE)
     print("player board created")
 
-    # declare bot instance
+    # randomly generate bot ship positions
     place_ships(bot_board)
     print("randomly generated bot ship locations")
+    print_ships(botShips)
 
     # bot chooses ship locations
 
